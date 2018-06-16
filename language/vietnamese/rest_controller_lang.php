29afb82ab877a789f991f7e939fc2b623426529e
Teddy NGUYEN GMAIL <chauminh88@gmail.com>
2018-06-16 11:29:23 +0700

    Added REST lang Vietnamese

---
+++ language/vietnamese/rest_controller_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * English language
+ */
+
+$lang['text_rest_invalid_api_key'] = 'API key không hợp lệ %s'; // %s is the REST API key
+$lang['text_rest_invalid_credentials'] = 'Xác thực tài khoản không hợp lệ';
+$lang['text_rest_ip_denied'] = 'Địa chỉ IP bị từ chối';
+$lang['text_rest_ip_unauthorized'] = 'Địa chỉ IP không được cho phép';
+$lang['text_rest_unauthorized'] = 'Không được phép';
+$lang['text_rest_ajax_only'] = 'Chỉ cho phép AJAX request';
+$lang['text_rest_api_key_unauthorized'] = ' API key này không có quyền truy cập';
+$lang['text_rest_api_key_permissions'] = 'API key này không đủ quyền hạn';
+$lang['text_rest_api_key_time_limit'] = 'API key này đã vượt quá giới hạn thời gian';
+$lang['text_rest_ip_address_time_limit'] = 'Địa chỉ IP này đã vượt quá giới hạn thời gian';
+$lang['text_rest_unknown_method'] = 'Phương thức chưa biết';
+$lang['text_rest_unsupported'] = 'Giao thức không được hỗ trợ';
